77cbc794e2b015093bab6d277e7e25099c78ff82
Kevin Willian <kevinwillian084@gmail.com>
2022-10-28 16:49:34 -0300

    feat: refatora componente Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,22 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
+import { useEffect, useRef, useState, useCallback } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+type IconBaseProps = {
+  size: number;
+};
+
+type InputProps = {
+  name: string;
+  icon: React.ComponentType<IconBaseProps>;
+};
+
+type InputRef = HTMLInputElement;
+
+function Input({ name, icon: Icon, ...rest }: InputProps) {
+  const inputRef = useRef<InputRef>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -48,6 +54,6 @@ const Input = ({ name, icon: Icon, ...rest }) => {
       />
     </Container>
   );
-};
+}
 
 export default Input;
